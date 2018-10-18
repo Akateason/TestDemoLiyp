40acb894c0f18d3c22fec48e64eb70c6018f71a9
谢天宸 <xietianchen@shimo.im>
2018-10-18 17:14:10 +0800

    Initial Commit

---
+++ TestYunpanSDKTests/TestYunpanSDKTests.m
@@ -0,0 +1,37 @@
+//
+//  TestYunpanSDKTests.m
+//  TestYunpanSDKTests
+//
+//  Created by teason23 on 2018/10/18.
+//  Copyright © 2018年 teason23. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestYunpanSDKTests : XCTestCase
+
+@end
+
+@implementation TestYunpanSDKTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
